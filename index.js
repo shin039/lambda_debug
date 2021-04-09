7f23f2a7514225e795d2f2ec4eb5215870213c0e
shin039 <life.syncopation.4003@gmail.com>
2021-04-09 12:59:19 +0900

    indent

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const s3  = new AWS.S3();
 
 exports.handler = async (event, context) => {
   console.log("# -----------------------------------------");
+
   const data = await s3.listBuckets().promise();
   data.Buckets.map(bucket => console.log("S3 Buckets::" + bucket.Name));
+
   console.log("# -----------------------------------------");
+
   console.log("event: ", event)
   console.log("context: ", context)
 
